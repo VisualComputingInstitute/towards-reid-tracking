a88504770a3115c2a56922fdb90fb1c6cfe8d4b7
Stefan Breuers <breuers@vision.rwth-aachen.de>
2017-03-25 20:59:52 +0100

    Fake News !!!

--- heatmap_2d_tracker.py
+++ heatmap_2d_tracker.py
@@ -118,7 +118,6 @@ track_id = 1
 dt = 1./SEQ_FPS
 
 track_lists = [[], [], [], [], [], [], [], []]
-already_tracked_ids = [[], [], [], [], [], [], [], []]
 
 dist_thresh = 100 #pixel #TODO: dependent on resolution
 
@@ -127,18 +126,42 @@ for icam in range(1, 8+1):
     makedirs(pjoin(args.outdir, 'camera{}'.format(icam)), exist_ok=True)
 
 
-def embed_image(image):
-    return None  # z.B. (30,60,128)
+class FakeNews:
+    def __init__(self):
+        self.already_tracked_ids = [[], [], [], [], [], [], [], []]
+
+    def embed_image(self, image):
+        return None  # z.B. (30,60,128)
 
 
-def search_person(image_embedding, person_embedding, fake_track_id, fake_curr_cam_dets):
-    id_heatmap = np.random.rand(*HEATMAP_SHAPE)
-    id_det_boxes = fake_curr_cam_dets['boxes'][fake_curr_cam_dets['TIDs'] == fake_track_id]
-    return heatmap_sampling_for_dets(id_heatmap, id_det_boxes)
+    def search_person(self, image_embedding, person_embedding, fake_track_id, fake_curr_cam_dets):
+        id_heatmap = np.random.rand(*HEATMAP_SHAPE)
+        id_det_boxes = fake_curr_cam_dets['boxes'][fake_curr_cam_dets['TIDs'] == fake_track_id]
+        return heatmap_sampling_for_dets(id_heatmap, id_det_boxes)
+
+
+    def personness(self, image, known_embeddings,
+                   fake_curr_dets, fake_icam):
+        curr_cam_dets = slice_all(fake_curr_dets, fake_curr_dets['Cams'] == fake_icam)
+        new_det_indices = np.where(np.logical_not(np.in1d(curr_cam_dets['TIDs'], self.already_tracked_ids[icam - 1])))[0]
+        new_heatmaps_and_ids = []
+        for each_det_idx in new_det_indices:
+            new_heatmap = np.random.rand(*HEATMAP_SHAPE)
+            new_heatmap = heatmap_sampling_for_dets(new_heatmap, [curr_cam_dets['boxes'][each_det_idx]])
+            new_id = curr_cam_dets['TIDs'][each_det_idx]
+            # TODO: get correct track_id (loop heatmap, instead of function call?# )
+            # TODO: get id_heatmap of that guy for init_heatmap
+            self.already_tracked_ids[icam - 1].append(new_id)
+            new_heatmaps_and_ids.append((new_heatmap, new_id))
+        return new_heatmaps_and_ids
+
 
 #@profile
 def main():
     tstart = time.time()
+
+    fake_neural_news_network = FakeNews()
+
     # ===Tracking fun begins: iterate over frames===
     # TODO: global time (duke)
     for curr_frame in range(49700, 227540+1):
@@ -149,15 +172,15 @@ def main():
         curr_dets = slice_all(dets, dets['GFIDs'] == curr_frame)
         num_curr_dets = len(curr_dets)
 
-        image_embeddings = list(map(embed_image, images))
+        image_embeddings = list(map(fake_neural_news_network.embed_image, images))
 
         for icam, track_list in zip(range(1, 8+1), track_lists):
             curr_cam_dets = slice_all(curr_dets, curr_dets['Cams'] == icam)
             ### A) update existing tracks
             for itracker, each_tracker in enumerate(track_list):
                 # get ID_heatmap
-                id_heatmap = search_person(image_embeddings[icam-1], each_tracker.embedding,
-                                           fake_track_id=each_tracker.track_id, fake_curr_cam_dets=curr_cam_dets)
+                id_heatmap = fake_neural_news_network.search_person(image_embeddings[icam-1], each_tracker.embedding,
+                                                                    fake_track_id=each_tracker.track_id, fake_curr_cam_dets=curr_cam_dets)
                 # ---PREDICT---
                 each_tracker.track_predict()
                 # ---UPDATE---
@@ -168,20 +191,13 @@ def main():
         ### B) get new tracks from general heatmap
         for icam in range(1, 8 + 1):
             # TODO: ID management (duke)
-            curr_cam_dets = slice_all(curr_dets, curr_dets['Cams'] == icam)
-            new_det_indices = np.where(np.logical_not(np.in1d(curr_cam_dets['TIDs'], already_tracked_ids[icam-1])))[0]
-            for each_det_idx in new_det_indices:
-                new_heatmap = np.random.rand(*HEATMAP_SHAPE)
-                #if icam == 2:
-                #    import ipdb ; ipdb.set_trace()
-                new_heatmap = heatmap_sampling_for_dets(new_heatmap, [curr_cam_dets['boxes'][each_det_idx]])
-                new_id = curr_cam_dets['TIDs'][each_det_idx]
+            for new_heatmap, new_id in fake_neural_news_network.personness(images[icam-1], known_embeddings=None,
+                                                                           fake_curr_dets=curr_dets, fake_icam=icam):
                 # TODO: get correct track_id (loop heatmap, instead of function call?# )
                 # TODO: get id_heatmap of that guy for init_heatmap
                 new_track = Track(dt, curr_frame, new_heatmap, images[icam-1], track_id=new_id,
                                   state_shape=STATE_SHAPE, output_shape=SEQ_SHAPE)
                 track_lists[icam-1].append(new_track)
-                already_tracked_ids[icam-1].append(new_id)
 
 
         ### C) further track-management
